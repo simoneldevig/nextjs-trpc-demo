1170415c923dc431f174812365e4b325e90c64d5
Simon Eldevig <Simon.eldevig@lego.com>
2023-04-28 08:23:49 +0200

    feat: migrate to appDir

---
+++ apps/nextjs/app/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Index: FC = () => {
+  return (
+    <h1>
+      <span> Hello there, </span>
+      Welcome nextjs 👋
+    </h1>
+  );
+}
+
+export default Index;
